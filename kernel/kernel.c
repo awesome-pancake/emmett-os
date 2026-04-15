d7f1b16439ddc26e56cd8e3df46a949b3a4beabb
awesome-pancake <emmetthoffman@icloud.com>
2026-04-15 10:58:59 -0600

    Added a fill screen function to kernel.h

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -1,7 +1,9 @@
 #include "kernel.h"
 
-int main(DISPLAY *display, MEMORY_DESCRIPTOR *memory_map){
-    // Fill in a pixel
-    *display->frame_buffer = 0xFF;
+int main(DISPLAY *display, MEMORY_DESCRIPTOR *memory_map)
+{
+    // Fills the screen with grey
+    fill_screen(display);
+
     for(;;);
 }
--- kernel/kernel.h
+++ kernel/kernel.h
@@ -11,7 +11,31 @@ typedef struct{
 } MEMORY_DESCRIPTOR;
 
 typedef struct{
+    uint8_t     blue;
+    uint8_t     green;
+    uint8_t     red;
+} COLOUR;
+
+typedef struct{
     uint8_t     *frame_buffer;
     uint32_t    horizontal_resolution;
     uint32_t    vertical_resolution;
 } DISPLAY;
+
+void fill_screen(DISPLAY *display)
+{
+    uint8_t *buffer = display->frame_buffer;
+    uint32_t width = display->horizontal_resolution;
+    uint32_t height = display->vertical_resolution;
+
+    // Fill screen with grey
+    for(int y=0; y<height; y++){
+        for(int x=0; x<4*width; x++){
+            // Avoid reserved bytes
+            if(x%4 != 4){
+                uint8_t *px = buffer + x + 4*y*width;
+                *px = 0xAA;
+            }
+        }
+    }
+}
