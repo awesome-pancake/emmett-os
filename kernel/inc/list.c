6302beeb7ca5d40a2c1d8da15740788150698803
awesome-pancake <emmetthoffman@icloud.com>
2026-04-22 23:31:05 -0600

    Added list.h for linked list data structures

--- kernel/inc/list.c
+++ kernel/inc/list.c
@@ -1,5 +1,25 @@
 #include <list.h>
 
-int comp_test(){
+int list_add(struct list *new, struct list *head)
+{
+    // Set new pointers
+    new->bk = head;
+    new->fd = head->fd;
+
+    // Set head pointers
+    head->fd->bk = new;
+    head->fd = new;
+    return 0;
+}
+
+int list_add_tail(struct list *new, struct list *head)
+{
+    // Set new pointers
+    new->fd = head;
+    new->bk = head->bk;
+
+    // Set head pointers
+    head->bk->fd = new;
+    head->bk = new;
     return 0;
 }
--- kernel/inc/list.h
+++ kernel/inc/list.h
@@ -1,8 +1,13 @@
 
-// Doubly linked list, similar to the linux kernel's implementation
+// Doubly linked list.
 struct list {
+    void            *container;     // Normally void pointers are a crime, but in this case the alternative is an evil macro.
     struct list     *fd;
     struct list     *bk;
 };
 
-int comp_test();
+// Adds a list node immediately following the list head
+int list_add(struct list *new, struct list *head);
+
+// Adds a list node at the end of a list
+int list_add_tail(struct list *new, struct list *head);
